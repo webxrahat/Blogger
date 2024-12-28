191dbd6d50d49fe1b0111500248e98f80b213fd7
Md Rahat Islam <islammdrahat308@gmail.com>
2024-12-28 22:16:20 +0600

    hy

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,24 @@
+import { Route, Routes } from "react-router";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import CreatePost from "./pages/CreatePost";
+import PostDetails from "./pages/PostDetails";
+import EditPost from "./pages/EditPost";
+import Myblogs from "./pages/Myblogs";
+import Profile from "./pages/Profile";
+
 export default function App() {
  return (
-  <h1 className="text-3xl font-bold text-red-100 underline">Hello world!</h1>
+  <Routes>
+   <Route path="/" element={<Home />} />
+   <Route path="/login" element={<Login />} />
+   <Route path="/register" element={<Register />} />
+   <Route path="/write" element={<CreatePost />} />
+   <Route path="/post/post/:id" element={<PostDetails />} />
+   <Route path="/edit/:id" element={<EditPost />} />
+   <Route path="/myblogs/:id" element={<Myblogs />} />
+   <Route path="/profile/:id" element={<Profile />} />
+  </Routes>
  );
 }
