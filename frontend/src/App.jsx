a171732449fe5f13e940e85cdfee2c373a103511
Md Rahat Islam <islammdrahat308@gmail.com>
2024-12-28 08:10:25 +0600

    hhj

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,3 +1,5 @@
 export default function App() {
- return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+ return (
+  <h1 className="text-3xl font-bold text-red-100 underline">Hello world!</h1>
+ );
 }
---
+++ frontend/src/components/HomePost.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const HomePost = () => {
+ return <div>HomePost</div>;
+};
+
+export default HomePost;
