0365faf54ecd381c6a88090ab1a7df5273e7a2ec
Md Rahat Islam <islammdrahat308@gmail.com>
2024-12-29 21:23:00 +0600

    df

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,7 +1,25 @@
 import React from "react";
+import { Link } from "react-router";
 
 function Navbar() {
- return <div>Navbar</div>;
+ return (
+  <div>
+   <div className="flex items-center justify-between px-6 md:px-[200px] py-4 bg-black text-white">
+    <h1 className="text-lg md:text-xl font-extrabold">
+     <Link to="/">Blogosphere</Link>
+    </h1>
+    {path === "/" && (
+     <div className="flex justify-center items-center space-x-0">
+      <input
+       type="text"
+       className="outline-none px-3 text-black bg-white rounded-s-xl"
+       placeholder="Search a post"
+      />
+     </div>
+    )}
+   </div>
+  </div>
+ );
 }
 
 export default Navbar;
