ef40e77304bb80469f17dde44f02b4b4a9e2fd48
Md Rahat Islam <islammdrahat308@gmail.com>
2025-01-08 21:30:48 +0600

    ggh

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,5 @@
-import { Route, Routes } from "react-router";
-import Home from "./pages/Home";
-import Login from "./pages/Login";
-import Register from "./pages/Register";
-import CreatePost from "./pages/CreatePost";
-import PostDetails from "./pages/PostDetails";
-import EditPost from "./pages/EditPost";
-import Myblogs from "./pages/Myblogs";
-import Profile from "./pages/Profile";
+import MasterLayout from "./routes/MasterLayout";
 
 export default function App() {
- return (
-  <Routes>
-   <Route path="/" element={<Home />} />
-  </Routes>
- );
+ return <MasterLayout />;
 }
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -4,20 +4,14 @@ import { Link } from "react-router";
 function Navbar() {
  return (
   <div>
-   <div className="flex items-center justify-between px-6 md:px-[200px] py-4 bg-black text-white">
-    <h1 className="text-lg md:text-xl font-extrabold">
-     <Link to="/">Blogosphere</Link>
-    </h1>
-    {path === "/" && (
-     <div className="flex justify-center items-center space-x-0">
-      <input
-       type="text"
-       className="outline-none px-3 text-black bg-white rounded-s-xl"
-       placeholder="Search a post"
-      />
-     </div>
-    )}
-   </div>
+   <p>Icon</p>
+   <p>Logo</p>
+   <ul>
+    <li>Mega Menu 1</li>
+    <li>Mega Menu 2</li>
+    <li>Mega Menu 3</li>
+   </ul>
+   <button>Login</button>
   </div>
  );
 }
