aae3f2d75c0eab456f8bcceeaa841fb7d6bb7961
Md Rahat Islam <islammdrahat308@gmail.com>
2025-01-28 08:44:30 +0600

    posts data showing

--- frontend/src/components/HomePost.jsx
+++ frontend/src/components/HomePost.jsx
@@ -1,7 +1,25 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 const HomePost = () => {
- return <div>HomePost</div>;
+ const [posts, setPosts] = useState([]);
+ useEffect(() => {
+  fetch("posts.json")
+   .then((res) => {
+    if (!res.ok) {
+     throw new Error("Failed to fetch data");
+    }
+    return res.json();
+   })
+   .then((data) => setPosts(data))
+   .catch((error) => console.error("Error fetching JSON:", error));
+ }, []);
+ console.log(posts);
+
+ return (
+  <div>
+   <p>{posts.length}</p>
+  </div>
+ );
 };
 
 export default HomePost;
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import HeroCarousel from "../components/HeroCarousel";
 import SearchCategory from "../components/SearchCategory";
+import HomePost from "../components/HomePost";
 
 const Home = () => {
  return (
   <div>
    <HeroCarousel />
+   <HomePost />
    <SearchCategory />
   </div>
  );
