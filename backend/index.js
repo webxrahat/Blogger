d4186aed073d7700141ceb4eac3fd78821af0976
Md Rahat Islam <islammdrahat308@gmail.com>
2025-02-02 23:36:12 +0600

    rt

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const posts = require("./posts.json");
 const cors = require("cors");
 const port = process.env.PORT || 5000;
 
@@ -8,6 +9,16 @@ app.use(cors());
 
 app.get("/", (req, res) => {
  res.send("Hello World!");
+});
+
+app.get("/posts", (req, res) => {
+ res.send(posts);
+});
+
+app.get("/posts/:id", (req, res) => {
+ const id = parseInt(req.params.id);
+ const post = posts.find((post) => post.id === id) || {};
+ res.send(post);
 });
 
 app.listen(port, () => {
